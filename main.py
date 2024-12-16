f85693cfdb0d42a33b194297400beca711de4c4c
wonderfau1t <wonderrfau1t@gmail.com>
2024-12-17 01:49:22 +0700

    initial commit

--- main.py
+++ main.py
@@ -46,27 +46,67 @@ def check_transactions(wallet_address):
     transactions = response.get('result')
     if transactions:
         last_tx = transactions[0]
+
         if last_transactions.get(wallet_address) != last_tx['hash'] and last_tx['txreceipt_status'] == '1':
-            if wallet_address == hot_wallet_address and last_tx['to'] != wallet_address.lower():
-                if last_tx['to'] != wallet_address.lower():
-                    return
             last_transactions[wallet_address] = last_tx['hash']
-            message = (
-                f"🔔 <b>Новая транзакция!</b>\n"
-                f"Кошелек: <code>{wallet_address}</code>\n"
-                f"Hash: <code>{last_tx['hash']}</code>\n"
-                f"Ссылка: <a href='https://www.ftnscan.com/tx/{last_tx['hash']}'>Посмотреть</a>"
-            )
+
+            if wallet_address == hot_wallet_address:
+                if last_tx['from'] == wallet_address.lower():
+                    message = (
+                        f"⬇️ <b>Вывод с MEXC</b>\n"
+                        f"На адрес: <code>{last_tx['to']}</code>\n"
+                        f"Сумма: {float(last_tx['value']) / 10 ** 18:.4f} FTN\n"
+                        f"Хэш: <code>{last_tx['hash']}</code>\n"
+                        f"Ссылка: <a href='https://www.ftnscan.com/tx/{last_tx['hash']}'>Посмотреть</a>"
+                    )
+                elif last_tx['to'] == wallet_address.lower():
+                    message = (
+                        f"⬆️ <b>Ввод на MEXC</b>\n"
+                        f"С адреса: <code>{last_tx['from']}</code>\n"
+                        f"Сумма: {float(last_tx['value']) / 10 ** 18:.4f} FTN\n"
+                        f"Хэш: <code>{last_tx['hash']}</code>\n"
+                        f"Ссылка: <a href='https://www.ftnscan.com/tx/{last_tx['hash']}'>Посмотреть</a>"
+                    )
+            elif wallet_address == contract_address and last_tx['input'].startswith('0x98dcef71'):
+                amount = get_amount_of_ftn(last_tx['hash'])
+                message = (
+                    f"️⚠️ <b>С контракта разлочены FTN</b>\n"
+                    f"Адрес: <code>{last_tx['from']}</code>\n"
+                    f"Сумма: {amount:.4f} FTN\n"
+                    f"Хэш: <code>{last_tx['hash']}</code>\n"
+                    f"Ссылка: <a href='https://www.ftnscan.com/tx/{last_tx['hash']}'>Посмотреть</a>"
+                )
+            else:
+                message = (
+                    f"🔔 <b>Что-то на промежуточном адресе происходит</b>\n"
+                    f"Адрес: <code>{wallet_address}</code>\n"
+                    f"Хэш: <code>{last_tx['hash']}</code>\n"
+                    f"Ссылка: <a href='https://www.ftnscan.com/tx/{last_tx['hash']}'>Посмотреть</a>"
+                )
             send_telegram_notification(message)
 
 
 def main():
+    send_telegram_notification('Перезапуск...\nПока без фильтра на суммы выводов, надо потестить пока что это :)')
     print("Запуск цикла...")
     while True:
         check_transactions(hot_wallet_address)
         check_transactions(middle_wallet_address)
         check_transactions(contract_address)
         time.sleep(5)
+
+
+def get_amount_of_ftn(tx_hash):
+    amount = 0
+    logs = requests.get("https://api.ftnscan.com/api", params={
+        'module': 'transaction',
+        'action': 'gettxinfo',
+        'txhash': tx_hash
+    }).json().get('result').get('logs')
+    for log in logs:
+        if log.get('topics')[0].startswith('0xddf252ad'):
+            amount = int(log.get('data'), 16) / 10 ** 18
+    return amount
 
 
 if __name__ == '__main__':
